b5bdcade2d1845f66be1610e7c0c02fc8172d5f6
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-08 08:35:25 +0700

    fix upload file for photo

--- controllers/photo.go
+++ controllers/photo.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"my-gram/models"
 	"my-gram/services"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,9 +19,7 @@ func NewPhotoController(service services.PhotoService) *photoController {
 }
 
 func (c *photoController) CreatePhoto(ctx *gin.Context) {
-	var input models.Photo
-
-	err := ctx.ShouldBindJSON(&input)
+	file, err := ctx.FormFile("photo_url")
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"messsage": "Bad request",
@@ -27,6 +27,32 @@ func (c *photoController) CreatePhoto(ctx *gin.Context) {
 		})
 		return
 	}
+
+	path := fmt.Sprintf("images/%s", file.Filename)
+	err = ctx.SaveUploadedFile(file, path)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"messsage": "Internal server error",
+			"error":    err.Error(),
+		})
+		return
+	}
+
+	var input models.Photo
+
+	input.PhotoUrl = path
+	title := ctx.PostForm("title")
+	caption := ctx.PostForm("caption")
+	userID := ctx.PostForm("user_id")
+
+	input.Title = title
+	input.Caption = caption
+	userIDUint, err := strconv.ParseUint(userID, 10, 64)
+	if err != nil {
+		fmt.Println("Failed to convert string to uint:", err)
+		return
+	}
+	input.UserID = uint(userIDUint)
 
 	newPhoto, err := c.service.CreatePhoto(input)
 	if err != nil {
--- models/photo.go
+++ models/photo.go
@@ -2,8 +2,8 @@ package models
 
 type Photo struct {
 	GORMModel
-	UserID   uint   `json:"user_id"`
-	Title    string `json:"title"`
-	Caption  string `json:"caption"`
-	PhotoUrl string `json:"photo_url"`
+	UserID   uint   `form:"user_id"`
+	Title    string `form:"title"`
+	Caption  string `form:"caption"`
+	PhotoUrl string `form:"photo_url"`
 }

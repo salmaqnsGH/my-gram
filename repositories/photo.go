9a4b733c2d6ca3abba638d1652ac816e8ed9b3b0
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-08 07:06:59 +0700

    fix repository name

--- repositories/photo.go
+++ repositories/photo.go
@@ -10,15 +10,15 @@ type PhotoRepository interface {
 	Create(photo models.Photo) (models.Photo, error)
 }
 
-type repository struct {
+type photoRepository struct {
 	db *gorm.DB
 }
 
-func NewPhotoRepository(db *gorm.DB) *repository {
-	return &repository{db}
+func NewPhotoRepository(db *gorm.DB) *photoRepository {
+	return &photoRepository{db}
 }
 
-func (r *repository) Create(photo models.Photo) (models.Photo, error) {
+func (r *photoRepository) Create(photo models.Photo) (models.Photo, error) {
 	err := r.db.Create(&photo).Error
 
 	if err != nil {

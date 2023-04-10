bf1b745ed869c2d3a9f5d10e123db4a084002a6d
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-10 19:23:57 +0700

    fix comment model and social_media model

--- models/comment.go
+++ models/comment.go
@@ -2,7 +2,7 @@ package models
 
 type Comment struct {
 	GORMModel
-	UserID  uint
-	PhotoID uint
-	message string
+	UserID  uint   `gorm:"not null" json:"user_id"`
+	PhotoID uint   `gorm:"not null" json:"photo_id"`
+	Message string `gorm:"not null" json:"message"`
 }
--- models/socialmedia.go
+++ models/socialmedia.go
@@ -2,7 +2,7 @@ package models
 
 type SocialMedia struct {
 	GORMModel
-	UserID         uint
-	name           string
-	SocialMediaUrl string
+	UserID         uint   `gorm:"not null" json:"user_id"`
+	Name           string `gorm:"not null" json:"name"`
+	SocialMediaUrl string `gorm:"not null" json:"social_media_url"`
 }

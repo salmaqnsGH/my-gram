c5a56e0cfca5c8d66b45f6f0fe16f34e03d84a53
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-12 15:41:17 +0700

    validate comment

--- models/comment.go
+++ models/comment.go
@@ -1,10 +1,17 @@
 package models
 
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
 type Comment struct {
 	GORMModel
 	UserID  uint   `gorm:"not null" json:"user_id"`
 	PhotoID uint   `gorm:"not null" json:"photo_id"`
-	Message string `gorm:"not null" json:"message"`
+	Message string `gorm:"not null" json:"message" validate:"required"`
 }
 
 type CreateCommentInput struct {
@@ -15,4 +22,15 @@ type CreateCommentInput struct {
 
 type UpdateCommentInput struct {
 	Message string `json:"message"`
+}
+
+func (c *Comment) BeforeCreate(tx *gorm.DB) (err error) {
+	validate := validator.New()
+
+	err = validate.Struct(c)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return
 }

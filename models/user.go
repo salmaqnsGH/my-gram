39a324e8e1472251e973db95f4063944da002d5e
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-12 13:43:48 +0700

    validate user

--- models/user.go
+++ models/user.go
@@ -1,9 +1,27 @@
 package models
 
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	GORMModel
-	Username string `gorm:"not null" json:"username"`
-	Email    string `gorm:"not null;uniqueIndex" json:"email"`
-	Password string `gorm:"not null" json:"password"`
-	Age      uint   `gorm:"not null" json:"age"`
+	Username string `gorm:"not null" json:"username" validate:"required"`
+	Email    string `gorm:"not null;uniqueIndex" json:"email" validate:"required,email"`
+	Password string `gorm:"not null" json:"password" validate:"required,min=6"`
+	Age      uint   `gorm:"not null" json:"age" validate:"required,gt=8"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	validate := validator.New()
+
+	err = validate.Struct(u)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return
 }

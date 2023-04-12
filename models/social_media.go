a40c0415a50c3e2118eea4a39d70c88d1fe75db1
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-04-12 15:38:53 +0700

    validate social media

--- models/social_media.go
+++ models/social_media.go
@@ -1,19 +1,37 @@
 package models
 
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
 type SocialMedia struct {
 	GORMModel
 	UserID         uint   `gorm:"not null" json:"user_id"`
-	Name           string `gorm:"not null" json:"name"`
-	SocialMediaUrl string `gorm:"not null" json:"social_media_url"`
+	Name           string `gorm:"not null" json:"name" validate:"required"`
+	SocialMediaUrl string `gorm:"not null" json:"social_media_url" validate:"required"`
 }
 
 type CreateSocialMediaInput struct {
-	UserID         uint   `gorm:"not null" json:"user_id"`
-	Name           string `gorm:"not null" json:"name"`
-	SocialMediaUrl string `gorm:"not null" json:"social_media_url"`
+	UserID         uint   `json:"user_id"`
+	Name           string `json:"name"`
+	SocialMediaUrl string `json:"social_media_url" validate:"required"`
 }
 
 type UpdateSocialMediaInput struct {
-	Name           string `gorm:"not null" json:"name"`
-	SocialMediaUrl string `gorm:"not null" json:"social_media_url"`
+	Name           string `json:"name"`
+	SocialMediaUrl string `json:"social_media_url"`
+}
+
+func (s *SocialMedia) BeforeCreate(tx *gorm.DB) (err error) {
+	validate := validator.New()
+
+	err = validate.Struct(s)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return
 }
